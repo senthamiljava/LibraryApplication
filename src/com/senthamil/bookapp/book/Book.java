cdfc765dd8dbd9eadb61c20f42eb33903c19eac4
Revature <Revature@Revature96589>
2017-01-02 06:58:33 -0500

    BookModel

---
+++ src/com/senthamil/bookapp/book/Book.java
@@ -0,0 +1,26 @@
+package com.senthamil.bookapp.book;
+
+public class Book {
+int book_id;
+String book_name;
+String author_name;
+public String getAuthor_name() {
+	return author_name;
+}
+public void setAuthor_name(String author_name) {
+	this.author_name = author_name;
+}
+public int getBook_id() {
+	return book_id;
+}
+public void setBook_id(int book_id) {
+	this.book_id = book_id;
+}
+public String getBook_name() {
+	return book_name;
+}
+public void setBook_name(String book_name) {
+	this.book_name = book_name;
+}
+
+}
